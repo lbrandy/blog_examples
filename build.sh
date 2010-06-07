9ae21136d96e9667af5d1cb1063a919416a6dca2
louis <louis@slim.(none)>
2010-06-06 22:33:51 -0400

    a new example, for a new blog post....

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-gcc opto.c -o slow
-gcc -O3 -funroll-loops opto.c -o fast
+#gcc opto.c -o opto_slow
+#gcc -O3 -funroll-loops opto.c -o fast_opto
+
+gcc signal_blur.c -o signal_blur_slow
+gcc -O3 signal_blur.c -o signal_blur_opt
---
+++ signal_blur.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LENGTH 5000
+
+// a bad impersonation of a gaussian filter
+const float filter[] = {0.01f, 0.2f, 0.58f, 0.2f, 0.01f};
+
+void naive(float* in, float* out)
+{
+  int i,j;
+  for (i=0;i<LENGTH-4;i++)
+  {
+    out[i]=0.0f;
+    for (j = 0;j<5;j++)
+      out[i] += filter[j] * in[i+j];
+  }
+}
+
+void naive_restrict(float *__restrict__ in, float *__restrict__ out)
+{
+  int i,j;
+  for (i=0;i<LENGTH-4;i++)
+  {
+    out[i]=0.0f;
+    for (j = 0;j<5;j++)
+      out[i] += filter[j] * in[i+j];
+  }
+}
+
+
+void sum_variable(float* in, float* out)
+{
+  int i,j;
+  for (i=0;i<LENGTH-4;i++)
+  {
+    float sum = 0.0f;
+    for (j = 0;j<5;j++)
+      sum += filter[j] * in[i+j];
+    out[i] = sum;
+  }
+}
+
+float elapsed(struct timeval before, struct timeval after)
+{
+  float ans = (float) (after.tv_sec - before.tv_sec);
+  ans += ((float) (after.tv_usec - before.tv_usec)) / 1000000;
+  return ans;
+}
+
+float gogo( void (*func)(float*,float*), float* in, float* out)
+{
+  struct timeval before,after;
+  int i;
+
+  func(in,out);
+  gettimeofday(&before,NULL);
+  for (i=0;i<10000;i++)
+    func(in,out);
+  gettimeofday(&after,NULL);
+
+  return elapsed(before,after);
+}
+
+
+int main(int argc, char *argv[])
+{
+  float *in;
+  float *ans;
+  int i;
+
+  in = valloc(sizeof(float) * LENGTH);
+  ans = valloc(sizeof(float) * LENGTH);
+
+  for (i=0;i<LENGTH;i++)
+    in[i]=(float) i;
+
+  printf("%s\n", argv[0]);
+  printf("  naive: %f\n", gogo(naive,in,ans));
+  printf("  naive_restrict: %f\n", gogo(naive_restrict,in,ans));
+  printf("  sum_variable: %f\n", gogo(sum_variable,in,ans));
+  printf("\n\n");
+  return 1;
+}
